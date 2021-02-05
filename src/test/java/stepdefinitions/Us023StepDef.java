264337f23b031ab878fef9de158da193897a267c
tugrulcetin <av.tugrulcetin@gmail.com>
2021-02-05 16:50:49 +0100

    5 subat by tugrul01

---
+++ src/test/java/stepdefinitions/Us023StepDef.java
@@ -0,0 +1,19 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class Us023StepDef {
+    @Given("kullanici api end pointi {string} kullanarak tum yanitlari set eder")
+    public void kullaniciApiEndPointiKullanarakTumYanitlariSetEder(String arg0) {
+    }
+
+    @And("kulllanici country verilerini json olarak java pojo ya cevirir")
+    public void kulllaniciCountryVerileriniJsonOlarakJavaPojoYaCevirir() {
+    }
+
+    @Then("kullanıcı verileri doğrular")
+    public void kullanıcıVerileriDoğrular() {
+    }
+}
